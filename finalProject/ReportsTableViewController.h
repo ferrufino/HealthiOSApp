97cd013b7cda05c29a7727c6f7b60e15d9bb1b7c
Eduardo Zardain <ejzardainc@gmail.com>
2015-11-04 23:56:43 -0600

    Adds ReportsViewController

---
+++ finalProject/ReportsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReportsTableViewController.h
+//  
+//
+//  Created by Lalo on 4/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReportsTableViewController : UITableViewController
+
+@end
