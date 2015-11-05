82c45c7144d5ab13ac8e05c11fcbcceca9fc92f6
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2015-11-05 11:13:43 -0600

    adds random line charts

--- finalProject/FirstViewController.m
+++ finalProject/FirstViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "FirstViewController.h"
+#import "FSLineChart.h"
+#import "UIColor+FSPalette.h"
 
 @interface FirstViewController ()
 
@@ -16,12 +18,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+  [self.view addSubview:[self chart1]];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(FSLineChart*)chart1 {
+    // Generating some dummy data
+    NSMutableArray* chartData = [NSMutableArray arrayWithCapacity:10];
+    
+    for(int i=0;i<10;i++) {
+        int r = (rand() + rand()) % 1000;
+        chartData[i] = [NSNumber numberWithInt:r + 200];
+    }
+    
+    // Creating the line chart
+    FSLineChart* lineChart = [[FSLineChart alloc] initWithFrame:CGRectMake(20, 400, [UIScreen mainScreen].bounds.size.width - 40, 166)];
+    lineChart.verticalGridStep = 5;
+    lineChart.horizontalGridStep = 9;
+    
+    lineChart.labelForIndex = ^(NSUInteger item) {
+        return [NSString stringWithFormat:@"%lu",(unsigned long)item];
+    };
+    
+    lineChart.labelForValue = ^(CGFloat value) {
+        return [NSString stringWithFormat:@"%.f", value];
+    };
+    
+    [lineChart setChartData:chartData];
+    return lineChart;
 }
 
 - (IBAction)btnAgrega:(UIButton *)sender {
