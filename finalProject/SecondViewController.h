ef2a42112a3e90fc52e7353d08616b9419cbe5a4
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2015-11-05 09:39:51 -0600

    adds pie chart

--- finalProject/SecondViewController.h
+++ finalProject/SecondViewController.h
@@ -7,9 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "XYPieChart.h"
+@interface SecondViewController : UIViewController <XYPieChartDelegate, XYPieChartDataSource>
 
-@interface SecondViewController : UIViewController
-
-
+@property (weak, nonatomic) IBOutlet XYPieChart *pieChart;
+@property (weak, nonatomic) IBOutlet XYPieChart *pieChart2;
+@property (strong, nonatomic) IBOutlet UILabel *percentageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *selectedSliceLabel;
+@property (strong, nonatomic) IBOutlet UITextField *numOfSlices;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *indexOfSlices;
+@property (strong, nonatomic) IBOutlet UIButton *downArrow;
+@property(nonatomic, strong) NSMutableArray *slices;
+@property(nonatomic, strong) NSArray        *sliceColors;
 @end
 
