54a4f10d092beb0d57a9bb7816e488dcea9d6d7d
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2015-11-09 21:57:04 -0600

    new header added

--- finalProject/FirstViewController.h
+++ finalProject/FirstViewController.h
@@ -7,12 +7,13 @@
 //
 
 
-#import <JBChart/JBChart.h>
-#import "AppDelegate.h"
-// Views
-@interface FirstViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet JBBarChartView *barChart;
+#import "AppDelegate.h"
+#import "BEMSimpleLineGraphView.h"
+// Views
+@interface FirstViewController : UIViewController<BEMSimpleLineGraphDataSource, BEMSimpleLineGraphDelegate>
+
+
 @property (weak, nonatomic) IBOutlet UILabel *lbInfo;
 - (IBAction)btnAgrega:(UIButton *)sender;
 - (IBAction)btnConfirma:(UIButton *)sender;
@@ -21,6 +22,15 @@
 @property (weak, nonatomic) IBOutlet UIView *FormaSue;
 @property (weak, nonatomic) IBOutlet UIButton *btnAddRecord;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+
+
+@property (weak, nonatomic) IBOutlet BEMSimpleLineGraphView *myGraph;
+@property (strong, nonatomic) NSMutableArray *arrayOfValues;
+@property (strong, nonatomic) NSMutableArray *arrayOfDates;
+@property (weak, nonatomic) IBOutlet UILabel *labelValues;
+@property (weak, nonatomic) IBOutlet UILabel *labelDates;
+
 
 @end
 
