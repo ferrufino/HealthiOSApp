184d1a8b8852accd887ac8c83bbad3cf82494077
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2015-11-13 00:06:41 -0600

    cleans some stuf

--- finalProject/ThirdViewController.m
+++ finalProject/ThirdViewController.m
@@ -8,7 +8,6 @@
 
 #import "ThirdViewController.h"
 #import "SecondViewController.h"
-#import "FSLineChart.h"
 #import "UIColor+FSPalette.h"
 
 #define TAG_First 1
@@ -22,7 +21,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self.view addSubview:[self chart3]];
+   
     
     
     
@@ -60,34 +59,7 @@
 - (IBAction)btnConfirma:(UIButton *)sender {
 
 }
--(FSLineChart*)chart3 {
-    // Generating some dummy data
-    NSMutableArray* chartData = [NSMutableArray arrayWithCapacity:7];
-    for(int i=0;i<7;i++) {
-        chartData[i] = [NSNumber numberWithFloat: (float)i / 30.0f + (float)(rand() % 100) / 500.0f];
-    }
-    
-    NSArray* months = @[@"January", @"February", @"March", @"April", @"May", @"June", @"July"];
-    
-    // Creating the line chart
-    FSLineChart* lineChart = [[FSLineChart alloc] initWithFrame:CGRectMake(20, 400, [UIScreen mainScreen].bounds.size.width - 40, 166)];
-    lineChart.verticalGridStep = 4;
-    lineChart.horizontalGridStep = 3; // 151,187,205,0.2
-    lineChart.color = [UIColor colorWithRed:151.0f/255.0f green:187.0f/255.0f blue:205.0f/255.0f alpha:1.0f];
-    lineChart.fillColor = [lineChart.color colorWithAlphaComponent:0.3];
-    
-    lineChart.labelForIndex = ^(NSUInteger item) {
-        return months[item];
-    };
-    
-    lineChart.labelForValue = ^(CGFloat value) {
-        return [NSString stringWithFormat:@"%.02f hrs", value];
-    };
-    
-    [lineChart setChartData:chartData];
-    
-    return lineChart;
-}
+
 
 
 - (IBAction)firstAlert {
