467d18aeccce54593d56ff39de37ae2c84dee04d
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2015-11-20 22:53:53 -0600

    adds core data and fixes horizontal scroll view and vertical view animation

--- finalProject/SecondViewController.m
+++ finalProject/SecondViewController.m
@@ -12,23 +12,23 @@
 @property NSMutableArray *answers;
 @property NSInteger numOfQuestion;
 @property NSManagedObject *lastRecord;
+@property NSArray *fetchResults;
 @end
 
 @implementation SecondViewController
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
 
+     [self setQuestionView];
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+   
     [self loadGraphData];
-    
-    
-    
-    
-    
-    
     [self loadQuestionView];
     
-    [self setQuestionView];
     
     /*****Vertical Scroll***/
     [self.verticalScroll setScrollEnabled:YES];
@@ -46,7 +46,52 @@
     [labelSuggestion setFont:[UIFont fontWithName:@"Avenir" size:15]];
     [testView addSubview:labelSuggestion];
     [self.verticalScroll addSubview:testView];
-
+    
+    
+    
+    //Graph
+    // Create a gradient to apply to the bottom portion of the graph
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    size_t num_locations = 2;
+    CGFloat locations[2] = { 0.0, 1.0 };
+    CGFloat components[8] = {
+        1.0, 1.0, 1.0, 1.0,
+        1.0, 1.0, 1.0, 0.0
+    };
+    // Apply the gradient to the bottom portion of the graph
+    self.nutriGraph.gradientBottom = CGGradientCreateWithColorComponents(colorspace, components, locations, num_locations);
+    
+    // Enable and disable various graph properties and axis displays
+    self.nutriGraph.enableTouchReport = YES;
+    self.nutriGraph.enablePopUpReport = YES;
+    self.nutriGraph.enableYAxisLabel = YES;
+    self.nutriGraph.autoScaleYAxis = YES;
+    self.nutriGraph.alwaysDisplayDots = NO;
+    self.nutriGraph.enableReferenceXAxisLines = YES;
+    self.nutriGraph.enableReferenceYAxisLines = YES;
+    self.nutriGraph.enableReferenceAxisFrame = YES;
+    self.nutriGraph.enableBezierCurve = YES;
+    
+    // Draw an average line
+    self.nutriGraph.averageLine.enableAverageLine = YES;
+    self.nutriGraph.averageLine.alpha = 0.6;
+    self.nutriGraph.averageLine.color = [UIColor darkGrayColor];
+    self.nutriGraph.averageLine.width = 2.5;
+    self.nutriGraph.averageLine.dashPattern = @[@(2),@(2)];
+    self.nutriGraph.colorXaxisLabel = [UIColor whiteColor];
+    self.nutriGraph.colorYaxisLabel = [UIColor whiteColor];
+    
+    // Set the graph's animation style to draw, fade, or none
+    self.nutriGraph.animationGraphStyle = BEMLineAnimationDraw;
+    
+    // Dash the y reference lines
+    self.nutriGraph.lineDashPatternForReferenceYAxisLines = @[@(2),@(2)];
+    
+    // Show the y axis values with this format string
+    self.nutriGraph.formatStringForValues = @"%.1f";
+    
+    
+    [self goAnimation];
 
 }
 
@@ -56,14 +101,98 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (void) setQuestionView {
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"FoodRecord"
+                                              inManagedObjectContext:context];
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entity];
+    
+    NSDate *today = [NSDate date];
+    NSDate *yesterday = [today dateByAddingTimeInterval:-86400.0];
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"date >= %@", yesterday];
+    [request setPredicate:predicate];
+    [request setFetchLimit:1];
+    
+    NSError *error;
+    _fetchResults = [context executeFetchRequest:request error:&error];
+    
+    if (_fetchResults.count != 0) {
+        self.horizontalScrollView.hidden = YES;
+        self.horizontalView.hidden = YES;
+        self.verticalScroll.frame = CGRectMake(0,65,self.verticalScroll.frame.size.width, self.verticalScroll.frame.size.height);
+    }else{
+        self.horizontalScrollView.hidden = NO;
+        self.horizontalView.hidden = NO;
+         self.verticalScroll.frame = CGRectMake(0,225,self.verticalScroll.frame.size.width, self.verticalScroll.frame.size.height);
+    
+    }
+    
+}
+
+
+- (void)loadGraphData {
+    NSDate *today = [NSDate date];
+    NSDate *yesterday = [today dateByAddingTimeInterval:-86400.0];
+    NSDate *twoDaysAgo = [yesterday dateByAddingTimeInterval:-86400.0];
+    NSDate *threeDaysAgo = [twoDaysAgo dateByAddingTimeInterval:-86400.0];
+    NSDate *fourDaysAgo = [threeDaysAgo dateByAddingTimeInterval:-86400.0];
+    NSDate *fiveDaysAgo = [fourDaysAgo dateByAddingTimeInterval:-86400.0];
+    NSDate *sixDaysAgo = [fiveDaysAgo dateByAddingTimeInterval:-86400.0];
+    NSDate *sevenDaysAgo = [sixDaysAgo dateByAddingTimeInterval:-86400.0];
+    
+    self.arrayOfDates = [[NSMutableArray alloc] initWithObjects:today, yesterday, twoDaysAgo,
+                         threeDaysAgo, fourDaysAgo, fiveDaysAgo, sixDaysAgo, sevenDaysAgo, nil];
+    
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *context = [appDelegate managedObjectContext];
+    
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"FoodRecord"
+                                              inManagedObjectContext:context];
+    NSFetchRequest *request = [[NSFetchRequest alloc] init];
+    [request setEntity:entity];
+    NSError *error;
+    NSManagedObject *record;
+    NSPredicate *predicate;
+    NSArray *fetchResults;
+    self.arrayOfValues = [[NSMutableArray alloc] init];
+    
+    for (int i = 0; i<=6; i++) {
+        predicate = [NSPredicate predicateWithFormat:@"(date <= %@) AND (date >= %@)",
+                     [self.arrayOfDates objectAtIndex:i], [self.arrayOfDates objectAtIndex:i+1]];
+        [request setPredicate:predicate];
+        [request setFetchLimit:1];
+        fetchResults = [context executeFetchRequest:request error:&error];
+        NSNumber *score = [[NSNumber alloc] initWithFloat:0];
+        if (fetchResults.count != 0) {
+            record = [fetchResults objectAtIndex:0];
+            score = [record valueForKey:@"score"];
+        }
+        [self.arrayOfValues addObject:score];
+    }
+    
+    [self.arrayOfDates removeObjectAtIndex:7];
+    self.arrayOfDates = [[[self.arrayOfDates reverseObjectEnumerator] allObjects] mutableCopy];
+    self.arrayOfValues = [[[self.arrayOfValues reverseObjectEnumerator] allObjects] mutableCopy];
+    
+    
+}
 
 - (void)loadQuestionView {
     self.numOfQuestion = 1;
     self.answers = [[NSMutableArray alloc] init];
     
+
+
+    
     self.horizontalScrollView.frame = CGRectMake(0, 0, 375, 162);
     //Impide que usuario pueda darle scroll
     self.horizontalScrollView.scrollEnabled = NO;
+    
+    
     
     //SegmentedControl for horizontal scroll view
     _segmentedControlQ1 = [[HMSegmentedControl alloc] initWithSectionTitles:@[@"1", @"2", @"3", @"4", @"5"]];
@@ -147,114 +276,26 @@
 
     [self.horizontalScrollView setContentSize:CGSizeMake(self.horizontalScrollView.frame.size.width*5, self.horizontalScrollView.frame.size.height)];
     
-    
-    
-    
-    /*****Vertical Scroll***/
-    [self.verticalScroll setScrollEnabled:YES];
-    [self.verticalScroll setContentSize:CGSizeMake(320, 800)];
-    
-    
-    //Animated cards
-    testView  = [[UIView alloc]initWithFrame:CGRectMake(65, 580, 280, 185)];
-    testView.backgroundColor = [UIColor redColor];
-    testView.layer.cornerRadius = 10.0; // set cornerRadius as you want.
-    testView.layer.borderColor = [UIColor lightGrayColor].CGColor; // set color as you want.
-    testView.layer.borderWidth = 1.0; // set borderWidth as you want.
-    
-    UILabel *labelSuggestion = [[UILabel alloc]initWithFrame:CGRectMake(50,30, 100, 100)];
-    [labelSuggestion setText:@"Sugerencia"];
-    [labelSuggestion setFont:[UIFont fontWithName:@"Avenir" size:15]];
-    [testView addSubview:labelSuggestion];
-    [self.verticalScroll addSubview:testView];
-    
-    
-    //Graph
-    
-    // Create a gradient to apply to the bottom portion of the graph
-    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
-    size_t num_locations = 2;
-    CGFloat locations[2] = { 0.0, 1.0 };
-    CGFloat components[8] = {
-        1.0, 1.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 0.0
-    };
-    // Apply the gradient to the bottom portion of the graph
-    self.nutriGraph.gradientBottom = CGGradientCreateWithColorComponents(colorspace, components, locations, num_locations);
-    
-    // Enable and disable various graph properties and axis displays
-    self.nutriGraph.enableTouchReport = YES;
-    self.nutriGraph.enablePopUpReport = YES;
-    self.nutriGraph.enableYAxisLabel = YES;
-    self.nutriGraph.autoScaleYAxis = YES;
-    self.nutriGraph.alwaysDisplayDots = NO;
-    self.nutriGraph.enableReferenceXAxisLines = YES;
-    self.nutriGraph.enableReferenceYAxisLines = YES;
-    self.nutriGraph.enableReferenceAxisFrame = YES;
-    self.nutriGraph.enableBezierCurve = YES;
-    
-    // Draw an average line
-    self.nutriGraph.averageLine.enableAverageLine = YES;
-    self.nutriGraph.averageLine.alpha = 0.6;
-    self.nutriGraph.averageLine.color = [UIColor darkGrayColor];
-    self.nutriGraph.averageLine.width = 2.5;
-    self.nutriGraph.averageLine.dashPattern = @[@(2),@(2)];
-    self.nutriGraph.colorXaxisLabel = [UIColor whiteColor];
-    self.nutriGraph.colorYaxisLabel = [UIColor whiteColor];
-    
-    // Set the graph's animation style to draw, fade, or none
-    self.nutriGraph.animationGraphStyle = BEMLineAnimationDraw;
-    
-    // Dash the y reference lines
-    self.nutriGraph.lineDashPatternForReferenceYAxisLines = @[@(2),@(2)];
-    
-    // Show the y axis values with this format string
-    self.nutriGraph.formatStringForValues = @"%.1f";
-    
 
-    [self goAnimation];
+    
 
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     
     self.tabBarController.navigationItem.rightBarButtonItem = nil;
-    if ([self.answers count] == 5){
-       // self.horizontalScrollView.hidden = YES;
-       // self.horizontalView.hidden = YES;
-        self.verticalScroll.frame = CGRectMake(0, 65, 375, 578);
-
-        
-    }
+//    if (_fetchResults.count != 0) {
+//        self.horizontalScrollView.hidden = YES;
+//        self.horizontalView.hidden = YES;
+//        self.verticalScroll.frame = CGRectMake(0,65,self.verticalScroll.frame.size.width, self.verticalScroll.frame.size.height);
+//        
+//        
+//    }
     
 
 }
 
-- (void) setQuestionView {
-     AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-     NSManagedObjectContext *context = [appDelegate managedObjectContext];
-     
-     NSEntityDescription *entity = [NSEntityDescription entityForName:@"FoodRecord"
-     inManagedObjectContext:context];
-     NSFetchRequest *request = [[NSFetchRequest alloc] init];
-     [request setEntity:entity];
-     
-     NSDate *today = [NSDate date];
-     NSDate *yesterday = [today dateByAddingTimeInterval:-86400.0];
-     
-     NSPredicate *predicate = [NSPredicate predicateWithFormat:@"date >= %@", yesterday];
-     [request setPredicate:predicate];
-     [request setFetchLimit:1];
-     
-     NSError *error;
-     NSArray *fetchResults = [context executeFetchRequest:request error:&error];
-     
-     if (fetchResults.count != 0) {
-         self.horizontalScrollView.hidden = YES;
-         self.verticalScroll.frame = CGRectMake(0,0,self.verticalScroll.frame.size.width, self.verticalScroll.frame.size.height);
-     }
 
-}
 
 #pragma mark Question Buttons
 
@@ -314,7 +355,7 @@
 //    CGRect frame = CGRectMake(self.horizontalScrollView.frame.size.width*5, 0, self.horizontalScrollView.frame.size.width, self.horizontalScrollView.frame.size.height); //wherever you want to scroll
 //    [self.horizontalScrollView scrollRectToVisible:frame animated:YES];
     self.horizontalScrollView.hidden = YES;
-    self.verticalScroll.frame = CGRectMake(0,0,self.verticalScroll.frame.size.width, self.verticalScroll.frame.size.height);
+    self.verticalScroll.frame = CGRectMake(0,65,self.verticalScroll.frame.size.width, self.verticalScroll.frame.size.height);
     for (id obj in self.answers) {
 
         NSLog(@"%@", obj);
@@ -510,52 +551,5 @@
 }
 
 
-
-- (void)loadGraphData {
-    NSDate *today = [NSDate date];
-    NSDate *yesterday = [today dateByAddingTimeInterval:-86400.0];
-    NSDate *twoDaysAgo = [yesterday dateByAddingTimeInterval:-86400.0];
-    NSDate *threeDaysAgo = [twoDaysAgo dateByAddingTimeInterval:-86400.0];
-    NSDate *fourDaysAgo = [threeDaysAgo dateByAddingTimeInterval:-86400.0];
-    NSDate *fiveDaysAgo = [fourDaysAgo dateByAddingTimeInterval:-86400.0];
-    NSDate *sixDaysAgo = [fiveDaysAgo dateByAddingTimeInterval:-86400.0];
-    NSDate *sevenDaysAgo = [sixDaysAgo dateByAddingTimeInterval:-86400.0];
-    
-    self.arrayOfDates = [[NSMutableArray alloc] initWithObjects:today, yesterday, twoDaysAgo,
-                         threeDaysAgo, fourDaysAgo, fiveDaysAgo, sixDaysAgo, sevenDaysAgo, nil];
-    
-    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-    NSManagedObjectContext *context = [appDelegate managedObjectContext];
-    
-    NSEntityDescription *entity = [NSEntityDescription entityForName:@"FoodRecord"
-                                              inManagedObjectContext:context];
-    NSFetchRequest *request = [[NSFetchRequest alloc] init];
-    [request setEntity:entity];
-    NSError *error;
-    NSManagedObject *record;
-    NSPredicate *predicate;
-    NSArray *fetchResults;
-    self.arrayOfValues = [[NSMutableArray alloc] init];
-    
-    for (int i = 0; i<=6; i++) {
-        predicate = [NSPredicate predicateWithFormat:@"(date <= %@) AND (date >= %@)",
-                     [self.arrayOfDates objectAtIndex:i], [self.arrayOfDates objectAtIndex:i+1]];
-        [request setPredicate:predicate];
-        [request setFetchLimit:1];
-        fetchResults = [context executeFetchRequest:request error:&error];
-        NSNumber *duration = [[NSNumber alloc] initWithFloat:0];
-        if (fetchResults.count != 0) {
-            record = [fetchResults objectAtIndex:0];
-            duration = [record valueForKey:@"duration"];
-        }
-        [self.arrayOfValues addObject:duration];
-    }
-    
-    [self.arrayOfDates removeObjectAtIndex:7];
-    self.arrayOfDates = [[[self.arrayOfDates reverseObjectEnumerator] allObjects] mutableCopy];
-    self.arrayOfValues = [[[self.arrayOfValues reverseObjectEnumerator] allObjects] mutableCopy];
-    
-    
-}
 
 @end
